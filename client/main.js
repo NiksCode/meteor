245621f3adf1945d3e0d38682a6a220679e3ee63
Nik Polovenko <npolovenko@nagios.internal>
2017-08-01 10:44:20 -0500

    start implementing the main page

--- client/main.js
+++ client/main.js
@@ -5,14 +5,19 @@ import {Meteor} from 'meteor/meteor';
 class HelloWorld extends React.Component {
   render() {
     return (
-      <div className="hello">
-        <h1>Shopping List for nik</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>Oculus</li>
-        </ul>
-      </div>
+      <div class="section no-pad-bot" id="index-banner">
+  <div class="container">
+
+    <h1 class="header center orange-text">Hello User</h1>
+    <div class="row center">
+      <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+    </div>
+    <div class="row center">
+      <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light  light-blue lighten-2">Get Started</a>
+    </div>
+  
+  </div>
+</div>
     );
   }
 }
